cd7893c974cc2ca2595fd90019faa9bd4d513232
Mohammad Rezazadeh <mrprocs@gmail.com>
2022-02-27 21:40:24 +0330

    Create a feature for adding months to a date

---
+++ back-end/helpers/date_time.py
@@ -0,0 +1,10 @@
+import calendar
+import datetime
+
+
+def add_months(date, months):
+    month = date.month - 1 + months
+    year = date.year + month // 12
+    month = month % 12 + 1
+    day = min(date.day, calendar.monthrange(year, month)[1])
+    return datetime.date(year, month, day)
